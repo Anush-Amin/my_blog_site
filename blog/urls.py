eb1f0eba85060de0be2f20f6f143b1ab651ac06f
Anush Amin <anush.mlore@gmail.com>
2023-09-02 23:42:12 +0530

    Commit after 58

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+# the variable name should be exactly urlpatterns or else will get error
+urlpatterns = [
+    path("", views.starting_page, name="starting-page"),
+    path("posts", views.posts, name="posts-page"),
+    path("posts/<slug:slug>", views.post_detail, 
+         name="post-detail-page")
+]
